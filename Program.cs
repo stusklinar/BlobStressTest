85b9244309d2049661ad48ce81ac0e8ff2b014fb
Stuart Sklinar <Stu@stuartalexanderltd.uk>
2023-10-03 14:12:05 +0100

    files

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using Azure.Identity;
 using Azure.Storage;
 using Azure.Storage.Blobs;
+using Azure.Storage.Files.Shares;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Client;
 using System.Diagnostics;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -23,19 +26,33 @@ namespace ConsoleApp1
 
             Console.WriteLine("Starting Test");
 
-            var client = new BlobServiceClient(new Uri(endpointUri),new StorageSharedKeyCredential(account,key));
-
+            var client = new BlobServiceClient(new Uri(endpointUri), new StorageSharedKeyCredential(account, key));
 
             var containerClient = client.GetBlobContainerClient(containerName);
 
             await containerClient.CreateIfNotExistsAsync();
 
-            await Upload(containerClient, 10L * 1024);
-            await Upload(containerClient, 50L * 1024);
-            await Upload(containerClient, 512L * 1024);
-            await Upload(containerClient, 512L * 1024 *1024,10);
+            //await Upload(containerClient, 10L * 1024);
+            //await Upload(containerClient, 50L * 1024);
+            //await Upload(containerClient, 512L * 1024);
+            //await Upload(containerClient, 512L * 1024 * 1024, 10);
 
-            await containerClient.DeleteAsync();
+            var filesConnStr = config["filesConnStr"];
+            var sharename = config["sharename"];
+
+
+
+            ShareServiceClient shareserviceclient = new ShareServiceClient(filesConnStr);
+            ShareClient shareclient = shareserviceclient.GetShareClient(sharename);
+            ShareDirectoryClient sharedirectoryclient = shareclient.GetDirectoryClient("test");
+
+            await sharedirectoryclient.CreateIfNotExistsAsync();
+
+
+            await Upload(sharedirectoryclient, 10L * 1024);
+            //await Upload(sharedirectoryclient, 50L * 1024);
+            //await Upload(sharedirectoryclient, 512L * 1024);
+            //await Upload(sharedirectoryclient, 512L * 1024 * 1024, 10);
 
         }
 
@@ -54,7 +71,7 @@ namespace ConsoleApp1
             var timer = Stopwatch.StartNew();
             for (int i = 0; i < iterations; i++)
             {
-                file.Seek(0, SeekOrigin.Begin); 
+                file.Seek(0, SeekOrigin.Begin);
                 await client.UploadBlobAsync($"{Guid.NewGuid()}.txt".ToString(), file);
 
             }
@@ -63,6 +80,23 @@ namespace ConsoleApp1
             Console.WriteLine($"Time Taken: {timer.Elapsed.TotalSeconds} - Size : {length} - Iterations : {iterations}");
         }
 
+        private async static Task Upload(ShareDirectoryClient client, long length, int iterations = 2000)
+        {
+            var file = CreateDummyFile(length); //10k
+            var timer = Stopwatch.StartNew();
+            for (int i = 0; i < iterations; i++)
+            {
+                file.Seek(0, SeekOrigin.Begin);
+
+                var fileClient = client.GetFileClient($"{Guid.NewGuid()}.txt".ToString());
+                await fileClient.CreateAsync(length);
+                fileClient.Upload(file);
+
+            }
+            timer.Stop();
+
+            Console.WriteLine($"Time Taken: {timer.Elapsed.TotalSeconds} - Size : {length} - Iterations : {iterations}");
+        }
 
     }
 }
